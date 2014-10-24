64cd15939ee9d5dc4c2e0ff7b5b445dfa29567a0
Matt Gauger <matt.gauger@gmail.com>
2014-10-24 14:58:54 -0500

    Add Grunt for copying files to deploy

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: 'sass/**/*.scss',
+        tasks: ['sass'],
+        options: {}
+      },
+      copyto: {
+        files: 'out/**',
+        tasks: ['copyto'],
+        options: {}
+      }
+    },
+    copyto: {
+      ghpages: {
+        files: [
+          {cwd: 'out/', src: ['**/*'], dest: 'gh-pages/', expand: true}
+        ],
+        options: {
+          // array of ignored paths, can be specific files or a glob
+          ignore: [
+            'stuffdir/**/*.bak',
+            'stuffdir/dontcopyme.txt',
+            // ignore both a directory and it's contents (brace expansion)
+            'stuffdir/somedir{,/**/*}'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['watch']);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-copy-to');
+};
